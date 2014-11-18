182e0240c21e4294810ef054b861d2ad8aec40c3
C457 <android.c357@gmail.com>
2014-11-17 23:35:43 -0600

    fascinatemtd: Update vendor blob extraction
    * This was outdated a long time ago
    * Remove blobs extracted in aries-common
    
    Change-Id: I882b2c890cd79939ca678d4ffc0c0ddf37d4f639

--- extract-files.sh
+++ extract-files.sh
@@ -1,41 +1,29 @@
-#!/bin/sh
+#!/bin/bash
 
 VENDOR=samsung
 DEVICE=fascinatemtd
 
-log() { printf %s\\n "$*"; }
-error() { log "ERROR: $@" >&2; }
-fatal() { error "$@"; exit 1; }
-try() { "$@" || fatal "'$*' failed"; }
-
-mydir=$(try dirname "$0") || exit 1
-try cd "${mydir}"
-
 BASE=../../../vendor/$VENDOR/$DEVICE/proprietary
-try rm -rf "${BASE}"
-CMD="adb pull "
+rm -rf $BASE/*
 
-if [ -e "$1" ]; then
-    try rm -rf tmp
-    try mkdir tmp
-    unzip -q "$1" -d tmp || fatal "Failed to unzip $1"
-    CMD="cp tmp"
-fi
+for FILE in `egrep -v '(^#|^$)' proprietary-files.txt`; do
+    OLDIFS=$IFS IFS=":" PARSING_ARRAY=($FILE) IFS=$OLDIFS
+    FILE=${PARSING_ARRAY[0]}
+    DEST=${PARSING_ARRAY[1]}
+    if [ -z $DEST ]
+    then
+        DEST=$FILE
+    fi
+    DIR=`dirname $FILE`
+    if [ ! -d $BASE/$DIR ]; then
+        mkdir -p $BASE/$DIR
+    fi
+    adb pull /system/$FILE $BASE/$DEST
+    # if file does not exist try destination
+    if [ "$?" != "0" ]
+    then
+        adb pull /system/$DEST $BASE/$DEST
+    fi
+done
 
-log "Copying proprietary files from device..."
-# first make sure we can read proprietary-files.txt
-proprietary_files_txt=$(try cat proprietary-files.txt) || exit 1
-# filter out comments and blank/whitespace-only lines
-printf %s\\n "${proprietary_files_txt}" |
-grep -Ev '^[[:space:]]*(#|$)' |
-while IFS= read -r FILE; do
-    DIR=$(try dirname "${FILE}") || exit 1
-    try mkdir -p "${BASE}"/"${DIR}"
-    log "  /system/${FILE}"
-    ${CMD}/system/"${FILE}" "${BASE}"/"${FILE}" \
-        || fatal "Failed to pull or copy $FILE"
-done || exit 1
-
-try rm -rf tmp
-
-./setup-makefiles.sh || exit 1
+./setup-makefiles.sh
